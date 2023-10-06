42a925139cf05f00ae7f92c20dded764983906a7
ahmadkhaleid <88000738+ahmadkhaleid@users.noreply.github.com>
2023-10-06 23:12:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,252 @@
+// Select Elements
+
+let category = document.querySelector(".category span");
+let countSpan = document.querySelector(".count span");
+let bullets = document.querySelector(".bullets");
+let bulletsSpanContainer = document.querySelector(".bullets .spans");
+let quizArea = document.querySelector(".quiz-area");
+let answersArea = document.querySelector(".answers-area");
+let submitButton = document.querySelector(".submit-button");
+let resultsContainer = document.querySelector(".results");
+let countdownElement = document.querySelector(".countdown");
+let jsBtn = document.querySelector(".js");
+let mathBtn = document.querySelector(".math");
+
+
+
+// Set Options
+let currentIndex = 0;
+let rightAnswers = 0;
+let countdownInterval;
+
+jsBtn.onclick = () => {
+    let quizUrl = "js_Q.json"
+    jsBtn.parentElement.remove()
+    category.innerHTML = "Java Script"
+    getQuestions(quizUrl);
+  }
+mathBtn.onclick = () => {
+    let quizUrl = "math_Q.json"
+    jsBtn.parentElement.remove()
+    category.innerHTML = "Math"
+    getQuestions(quizUrl);
+  }
+
+
+function getQuestions(quizUrl) {
+  let myRequest = new XMLHttpRequest();
+  myRequest.open("GET",quizUrl,true);
+  myRequest.send();
+
+  myRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let questionsObjectBeforeShuffel = JSON.parse(this.responseText);
+      let qCount = 6;
+      let questionsObject = shuffling(questionsObjectBeforeShuffel);
+
+      // Create Bullets + Set Questions Count
+      createBullets(qCount);
+
+      // Add Question Data
+      addQuestionData(questionsObject[currentIndex], qCount);
+
+      // Start CountDown
+      countdown(70, qCount);
+
+      // Click On Submit
+      submitButton.onclick = () => {
+        // Get Right Answer
+        let theRightAnswer = questionsObject[currentIndex].right_answer;
+
+        // Increase Index
+        currentIndex++;
+
+        // Check The Answer
+        checkAnswer(theRightAnswer, qCount);
+
+        // Remove Previous Question
+        quizArea.innerHTML = "";
+        answersArea.innerHTML = "";
+
+        // Add Question Data
+        addQuestionData(questionsObject[currentIndex], qCount);
+
+        // Handle Bullets Class
+        handleBullets();
+
+        // Start CountDown
+        clearInterval(countdownInterval);
+        countdown(70, qCount);
+
+        // Show Results
+        showResults(qCount);
+      };
+    }
+  };
+}
+
+
+function createBullets(num) {
+  countSpan.innerHTML = num;
+
+  // Create Spans
+  for (let i = 0; i < num; i++) {
+    // Create Bullet
+    let theBullet = document.createElement("span");
+
+    // Check If Its First Span
+    if (i === 0) {
+      theBullet.className = "on";
+    }
+
+    // Append Bullets To Main Bullet Container
+    bulletsSpanContainer.appendChild(theBullet);
+  }
+}
+
+function addQuestionData(obj, count) {
+  if (currentIndex < count) {
+    // Create H2 Question Title
+    let questionTitle = document.createElement("h2");
+
+    // Create Question Text
+    let questionText = document.createTextNode(obj["title"]);
+
+    // Append Text To H2
+    questionTitle.appendChild(questionText);
+
+    // Append The H2 To The Quiz Area
+    quizArea.appendChild(questionTitle);
+
+    //shuffling answers before Looping
+    let answersArray = [obj.answer_1, obj.answer_2, obj.answer_3, obj.answer_4]
+    let shuffledAnswers = shuffling(answersArray)
+    for (let i = 0; i < 4; i++) {
+      obj[`answer_${i}`] = shuffledAnswers[i]
+    }
+
+    // Create The Answers
+    for (let i = 1; i <= 4; i++) {
+      // Create Main Answer Div
+      let mainDiv = document.createElement("div");
+
+      // Add Class To Main Div
+      mainDiv.className = "answer";
+
+      // Create Radio Input
+      let radioInput = document.createElement("input");
+
+      // Add Type + Name + Id + Data-Attribute
+      radioInput.name = "question";
+      radioInput.type = "radio";
+      radioInput.id = `answer_${i}`;
+      radioInput.dataset.answer = obj[`answer_${i}`];
+
+      // Make First Option Selected
+      if (i === 1) {
+        radioInput.checked = true;
+      }
+
+      // Create Label
+      let theLabel = document.createElement("label");
+
+      // Add For Attribute
+      theLabel.htmlFor = `answer_${i}`;
+
+      // Create Label Text
+      let theLabelText = document.createTextNode(obj[`answer_${i}`]);
+
+      // Add The Text To Label
+      theLabel.appendChild(theLabelText);
+
+      // Add Input + Label To Main Div
+      mainDiv.appendChild(radioInput);
+      mainDiv.appendChild(theLabel);
+
+      // Append All Divs To Answers Area
+      answersArea.appendChild(mainDiv);
+    }
+  }
+}
+
+function checkAnswer(rAnswer, count) {
+  let answers = document.getElementsByName("question");
+  let theChoosenAnswer;
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i].checked) {
+      theChoosenAnswer = answers[i].dataset.answer;
+    }
+  }
+
+  if (rAnswer === theChoosenAnswer) {
+    rightAnswers++;
+  }
+}
+
+function handleBullets() {
+  let bulletsSpans = document.querySelectorAll(".bullets .spans span");
+  let arrayOfSpans = Array.from(bulletsSpans);
+  arrayOfSpans.forEach((span, index) => {
+    if (currentIndex === index) {
+      span.className = "on";
+    }
+  });
+}
+
+function showResults(count) {
+  let theResults;
+  if (currentIndex === count) {
+    quizArea.remove();
+    answersArea.remove();
+    submitButton.remove();
+    bullets.remove();
+
+    if (rightAnswers > count / 2 && rightAnswers < count) {
+      theResults = `<span class="good">Good</span>, ${rightAnswers} From ${count}`;
+    } else if (rightAnswers === count) {
+      theResults = `<span class="perfect">Perfect</span>, All Answers Is Good`;
+    } else {
+      theResults = `<span class="bad">Bad</span>, ${rightAnswers} From ${count}`;
+    }
+
+    resultsContainer.innerHTML = theResults;
+    resultsContainer.style.padding = "10px";
+    resultsContainer.style.backgroundColor = "white";
+    resultsContainer.style.marginTop = "10px";
+  }
+}
+
+function countdown(duration, count) {
+  if (currentIndex < count) {
+    let minutes, seconds;
+    countdownInterval = setInterval(function () {
+      minutes = parseInt(duration / 60);
+      seconds = parseInt(duration % 60);
+
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+      countdownElement.innerHTML = `${minutes}:${seconds}`;
+
+      if (--duration < 0) {
+        clearInterval(countdownInterval);
+        submitButton.click();
+      }
+    }, 1000);
+  }
+}
+
+function shuffling(array) {
+  let current = array.length ,
+  temp,
+  random
+  while (current > 0) {
+      random = Math.floor(Math.random()*current)
+      current--
+      temp = array[current]
+      array[current] = array[random]
+      array[random] = temp   
+  }
+  return array
+}
